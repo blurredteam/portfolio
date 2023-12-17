a3d99f9b2ff4af20e11f29b30a62255189d76518
LucasAH9 <lucasandosilla@gmail.com>
2023-12-17 20:58:36 +0100

    Añadida carta de Florecia
    - Añadida carta de florecia
    - Corregidas faltas de ortografía
    - Ajustados algunos textos

--- js/showCard.js
+++ js/showCard.js
@@ -1,6 +1,6 @@
     let currentCardIndex = 0;
-    const backImages = ['./assets/img/targetas/1.png', './assets/img/targetas/2.png', './assets/img/targetas/3.png', './assets/img/targetas/4.png', './assets/img/targetas/5.png']; // Replace with your image URLs
-    const frontImages  = ['./assets/img/targetas/1-1.png', './assets/img/targetas/2-1.png', './assets/img/targetas/3-1.png', './assets/img/targetas/4-1.png', './assets/img/targetas/5-1.png']; // Replace with your image URLs
+    const backImages = ['./assets/img/targetas/1.png', './assets/img/targetas/2.png', './assets/img/targetas/3.png', './assets/img/targetas/4.png', './assets/img/targetas/5.png', './assets/img/targetas/6.png']; // Replace with your image URLs
+    const frontImages  = ['./assets/img/targetas/1-1.png', './assets/img/targetas/2-1.png', './assets/img/targetas/3-1.png', './assets/img/targetas/4-1.png', './assets/img/targetas/5-1.png', './assets/img/targetas/6-1.png']; // Replace with your image URLs
     
     function showCard(direction) {
         if (direction === 'next') {
